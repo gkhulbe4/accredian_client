c8d1fbc4458cb8a7f9d6d88d1eaa1069b4919395
gkhulbe4 <khulbegarvit4@gmail.com>
2024-07-07 21:41:20 +0530

    small changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,10 +5,10 @@ function Hero() {
     <div className=" flex justify-center items-center">
       <div className="w-[80%] bg-[#eff5ff] flex flex-col lg:flex-row justify-between items-center py-10 px-8 rounded-2xl gap-3">
         <div className="w-[50%] flex flex-col gap-8">
-          <h1 className="text-7xl font-bold">Lets Learn & Earn</h1>
-          <p className="text-2xl font-light">
+          <h1 className="text-4xl lg:text-7xl font-bold">Lets Learn & Earn</h1>
+          <p className="text-md lg:text-2xl font-light">
             Get a chances to win upto <br />
-            <span className="text-[#1b70e2] font-medium text-4xl">
+            <span className="text-[#1b70e2] font-medium text-lg lg:text-4xl">
               Rs.15,000
             </span>
           </p>
